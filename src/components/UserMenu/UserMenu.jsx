49bd9870ea092d54896d6388b5d265c159205749
VladislavVolyanskiy <vladislavvolyanskiy@gmail.com>
2024-07-11 15:43:35 +0300

    useEffect used in UserMenu component with redirect state tracking out of hook

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,20 +1,26 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { selectUser } from "../../redux/auth/selectors";
 import { logout } from "../../redux/auth/operations";
-import {useNavigate} from 'react-router-dom'
-
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const UserMenu = () => {
   const { name } = useSelector(selectUser);
+  const [redirect, setRedirect] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleLogOut = () => {
-      dispatch(logout()).then(() => {
-      navigate("/login");
+    dispatch(logout()).then(() => {
+      setRedirect(true);
     });
   };
 
+  useEffect(() => {
+    if (redirect) {
+      navigate("/login");
+    }
+  }, [redirect, navigate]);
 
   return (
     <div>
@@ -23,6 +29,5 @@ const UserMenu = () => {
     </div>
   );
 };
-
 
 export default UserMenu;
