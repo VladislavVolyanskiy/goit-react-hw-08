d3c3e63a3d05dfce19efc0130f239b79f39ab340
VladislavVolyanskiy <vladislavvolyanskiy@gmail.com>
2024-07-11 15:21:21 +0300

    user redirected without useEffect

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,25 +1,19 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { selectUser } from "../../redux/auth/selectors";
 import { logout } from "../../redux/auth/operations";
-import { useEffect, useState } from 'react';
 import {useNavigate} from 'react-router-dom'
+
 
 const UserMenu = () => {
   const { name } = useSelector(selectUser);
-  const [redirect, setRedirect] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-    const handleLogOut = () => {
-      dispatch(logout());
+  const handleLogOut = () => {
+      dispatch(logout()).then(() => {
+      navigate("/login");
+    });
   };
-
-useEffect(() => {
-  if (redirect) {
-    navigate("/login");
-    setRedirect(true)
-  }
-}, [redirect, navigate]);
 
       
   return (
